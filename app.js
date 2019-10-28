c7c7137a3bcd4bfaa886a23d1002203f05d22741
CPrich905 <charlie.prichard@gmail.com>
2019-10-28 17:25:39 +0000

    linter errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function calcSave() {
-  let savings = +document.getElementById("savings").value
-  let interest = +document.getElementById("savings-interest").value
+  let savings = +document.getElementById('savings').value
+  let interest = +document.getElementById('savings-interest').value
   let result = savings * interest
   let total = savings += result
   savingsInterestYr.innerHTML = result
@@ -8,17 +8,17 @@ function calcSave() {
 }
 
 function calcRent() {
-  let rent = +document.getElementById("rent").value
-  let bills = +document.getElementById("bills").value
+  let rent = +document.getElementById('rent').value
+  let bills = +document.getElementById('bills').value
   let rbMonth = rent += bills
   let resultRent = rbMonth * 12
   owner1yr.innerHTML = resultRent
 }
 
 function calcMort() {
-  let houseUpkeek = +document.getElementById("upkeep-costs").value
-  let mortgageMonth = +document.getElementById("mortgage-monthly").value
-  let bills = +document.getElementById("bills").value
+  let houseUpkeek = +document.getElementById('upkeep-costs').value
+  let mortgageMonth = +document.getElementById('mortgage-monthly').value
+  let bills = +document.getElementById('bills').value
   let ownerMonth = mortgageMonth += bills
   let resultMortgage = ownerMonth * 12
   owner1yr.innerHTML = resultMortgage += houseUpkeek
--- buy_vs_rent.js
+++ buy_vs_rent.js
@@ -1,189 +1,195 @@
 // Accounts object
 function Accounts({
-  savings_start = 15000,
-  savings_interest = 0.07,
-  savings_payments = 0,
-  expendible_income_increase = 0,
-  rent_payments = 0,
-  rent_growth = 0,
-  mortgage_start = 0,
-  mortgage_interest = 0.025,
-  mortgage_payments = 0,
-  house_value = 0,
-  house_growth = 0.03
+  savingsStart = 15000,
+  savingsInterest = 0.07,
+  savingsPayments = 0,
+  expendibleIncomeIncrease = 0,
+  rentPayments = 0,
+  rentGrowth = 0,
+  mortgageStart = 0,
+  mortgageInterest = 0.025,
+  mortgagePayments = 0,
+  houseValue = 0,
+  houseGrowth = 0.03
 } = {}) {
   // set up default values
-  this.savings_start = savings_start;
-  this.savings_interest = savings_interest;
-  this.savings_payments = savings_payments;
-  this.expendible_income_increase = expendible_income_increase;
-  this.rent_payments = rent_payments;
-  this.rent_growth = rent_growth;
-  this.mortgage_start = mortgage_start;
-  this.mortgage_interest = mortgage_interest;
-  this.mortgage_payments = mortgage_payments;
-  this.house_value = house_value;
-  this.house_growth = house_growth;
+  this.savingsStart = savingsStart
+  this.savingsInterest = savingsInterest
+  this.savingsPayments = savingsPayments
+  this.expendibleIncomeIncrease = expendibleIncomeIncrease
+  this.rentPayments = rentPayments
+  this.rentGrowth = rentGrowth
+  this.mortgageStart = mortgageStart
+  this.mortgageInterest = mortgageInterest
+  this.mortgagePayments = mortgagePayments
+  this.houseValue = houseValue
+  this.houseGrowth = houseGrowth
 
-  this.savings = savings_start;
-  this.savings_paid = 0;
-  this.mortgage = mortgage_start;
-  this.mortgage_paid = 0;
+  this.savings = savingsStart
+  this.savings_paid = 0
+  this.mortgage = mortgageStart
+  this.mortgagePaid = 0
 
   // set up the object methods
   this.step = function() {
     // Step a single year
-    one_off_savings = 0;
+    oneOffSavings = 0
 
     // Step 1 month at a time, as savings often compound monthly
     for (i = 0; i < 12; i++) {
       ///// MORTGAGE
       // Calculate mortage interest
-      m_interest = this.mortgage * (this.mortgage_interest / 12);
-      this.mortgage += m_interest;
+      mInterest = this.mortgage * (this.mortgageInterest / 12);
+      this.mortgage += mInterest
 
       // Subtract payments
-      this.mortgage -= this.mortgage_payments;
-      this.mortgage_paid += this.mortgage_payments;
+      this.mortgage -= this.mortgagePayments
+      this.mortgagePaid += this.mortgagePayments
 
       // The month the mortgage is paid off, put everything into savings
-      if ((this.mortgage <= 0) & (this.mortgage_payments != 0)) {
-        one_off_savings = -this.mortgage;
-        this.savings_payments += this.mortgage_payments;
-        this.mortgage_payments = 0;
-        this.mortgage = 0;
+      if ((this.mortgage <= 0) & (this.mortgagePayments != 0)) {
+        oneOffSavings = -this.mortgage
+        this.savingsPayments += this.mortgagePayments
+        this.mortgagePayments = 0
+        this.mortgage = 0
       }
 
       /// SAVINGS
       // Add savings interest
-      s_interest = this.savings * (this.savings_interest / 12);
-      this.savings += s_interest;
+      sInterest = this.savings * (this.savingsInterest / 12)
+      this.savings += sInterest
 
       // Monthly contribution + leftovers in case mortgage was paid
-      this.savings += this.savings_payments + one_off_savings;
-      this.savings_paid += this.savings_payments + one_off_savings;
-      one_off_savings = 0;
+      this.savings += this.savingsPayments + oneOffSavings
+      this.savings_paid += this.savingsPayments + oneOffSavings
+      oneOffSavings = 0
     }
 
     // Update house price (annually, it doesn't compound)
-    house_value_accrued = this.house_value * this.house_growth;
-    this.house_value += house_value_accrued;
+    houseValueAccrued = this.houseValue * this.houseGrowth
+    this.houseValue += houseValueAccrued
 
     // Update total assets
-    this.update_total_assets();
-  };
+    this.updateTotalAssets()
+  }
 
-  this.update_total_assets = function() {
-    total_assets = 0;
+  this.updateTotalAssets = function() {
+    totalAssets = 0
 
-    if (this.mortgage_paid > 0) {
-      house_fraction_owned =
-        1 - this.mortgage / (this.mortgage + this.mortgage_paid);
-      this.house_percent_owned = house_fraction_owned * 100;
+    if (this.mortgagePaid > 0) {
+      houseFractionOwned =
+        1 - this.mortgage / (this.mortgage + this.mortgagePaid)
+      this.housePercentOwned = houseFractionOwned * 100
 
-      total_assets += house_fraction_owned * this.house_value;
+      totalAssets += houseFractionOwned * this.houseValue
     }
 
-    total_assets += this.savings - this.mortgage;
-    this.total_assets = total_assets;
-  };
+    totalAssets += this.savings - this.mortgage
+    this.totalAssets = totalAssets
+  }
 
   // function multplying inputs by years
   this.step_n_years = function(years) {
     for (year = 0; year < years; year++) {
-      this.step();
+      this.step()
     }
-  };
+  }
   // function to return results in console
-  this.pretty_print = function() {
-    let to_print = ["mortgage", "savings", "total_assets"];
+  this.prettyPrint = function() {
+    const toPrint = ['mortgage', 'savings', 'totalAssets']
 
-    console.log("*".repeat(70));
-    for (let k of to_print) {
+    console.log('*'.repeat(70))
+    for (let k of toPrint) {
       // Format v
-      let v = this[k];
-      v = v.toFixed(2).toLocaleString();
-      v = v.toString().padStart(20, " ");
+      let v = this[k]
+      v = v.toFixed(2).toLocaleString()
+      v = v.toString().padStart(20, ' ')
 
       // Format k
-      k = k.padEnd(50, " ");
-      let info = `${k}${v}`;
-      console.log(info);
+      k = k.padEnd(50, ' ')
+      let info = `${k}${v}`
+      console.log(info)
     }
-  };
 
-  this.populate_results = function() {
-    // console.log('firing')
-    //function to populate results on page
-    mortgage_result.innerHTML = this.mortgage.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
-    owner_savings_result.innerHTML = this.savings.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
-    owner_total_assets_result.innerHTML = this.total_assets.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
-    // rent_payments_result.innerHTML = 
-    // rent_savings_result.innerHTML = 
-    // rent_total_assets_result.innerHTML = 
+
   }
+
+  this.populateResults = function() {
+    console.log('populateResults firing')
+    //function to populate results on page
+    mortgageResult.innerHTML = this.mortgage.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
+    ownerSavingsResult.innerHTML = this.savings.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
+    ownerTotalAssetsResult.innerHTML = this.totalAssets.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'})
+    // rentPaymentsResult.innerHTML = this.
+    // rentSavingsResult.innerHTML =
+    // rentTotalAssetsResult.innerHTML =
+  }
+
+
 
 }
 
 function test() {
   // Check results match python with some sensible parameters
-  let years = 10;
-  let initial_investment = 50000;
-  let house_value = 400000;
-  let mortgage_start = house_value - initial_investment;
+  let years = 10
+  let initialInvestment = 50000
+  let houseValue = 400000
+  let mortgageStart = houseValue - initialInvestment
 
   // Rent type account
   let account = new Accounts({
-    savings_payments: 800,
-    rent_payments: 1200,
-    savings_start: initial_investment
-  });
-  account.step_n_years(years);
-  account.pretty_print();
+    savingsPayments: 800,
+    rentPayments: 1200,
+    savingsStart: initialInvestment
+  })
+  account.step_n_years(years)
+  account.prettyPrint()
+
 
   // Mortgage type account
   account = new Accounts({
-    savings_payments: 800,
-    mortgage_payments: 1200,
-    mortgage_start: mortgage_start,
-    house_value: house_value
-  });
-  account.step_n_years(years);
-  account.pretty_print();
+    savingsPayments: 800,
+    mortgagePayments: 1200,
+    mortgageStart: mortgageStart,
+    houseValue: houseValue
+  })
+  account.step_n_years(years)
+  account.prettyPrint()
 }
 
-test();
+test()
 
-function run_numbers() {
+function runNumbers() {
 // CONNECTS TO FRONT
-  
-//COMMON VALUES
-let years = document.getElementById('years').value;
-let house_value = +document.getElementById('house_value').value;
-let savings_start = +document.getElementById('savings_start').value
-let mortgage_start = (house_value - savings_start);
+  console.log('runNumbers firing')
+  //COMMON VALUES
+  let years = document.getElementById('years').value
+  let houseValue = +document.getElementById('houseValue').value
+  let savingsStart = +document.getElementById('savingsStart').value
+  let mortgageStart = (houseValue - savingsStart)
 
 
-// rental account
-let account = new Accounts({
-  savings_payments: +document.getElementById('savings_payments').value,
-  rent_payments: +document.getElementById('rent_payments'),
-  savings_start: savings_start,
-});
-account.step_n_years(years)
-account.pretty_print();
-account.populate_results();
+  // rental account - moved to rentNumbers
+  // buyer account
+  account = new Accounts({
+    savingsPayments: +document.getElementById('savingsPayments').value,
+    mortgagePayments: +document.getElementById('savingsPayments').value,
+    mortgageStart: mortgageStart,
+    houseValue: houseValue
+  })
+  account.step_n_years(years)
+  account.prettyPrint()
+  account.populateResults()
+}
 
-// buyer account 
-account = new Accounts({
-  savings_payments: +document.getElementById('savings_payments').value,
-  mortgage_payments: +document.getElementById('savings_payments').value,
-  mortgage_start: mortgage_start,
-  house_value: house_value,
-});
-account.step_n_years(years)
-account.pretty_print()
-account.populate_results();
-
-
+function rentNumbers() {
+  console.log('rent numbers')
+  let account = new Accounts({
+    savingsPayments: +document.getElementById('savingsPayments').value,
+    rentPayments: +document.getElementById('rentPayments'),
+    savingsStart: savingsStart
+  })
+  account.step_n_years(years)
+  account.prettyPrint()
+  account.populateResults()
 }
